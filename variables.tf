eb5eb440e236d5b97475fb96d2cd7d79217b90c2
dspriyasg <priya9.sdr@gmail.com>
2024-04-08 10:04:36 +0800

    add vault service

--- variables.tf
+++ variables.tf
@@ -7,9 +7,14 @@ variable "tools" {
 
     grafana = {
       instance_type = "t3.small"
-      policy_resource_list = ["ec2:DescribeInstances"]
+      policy_resource_list = []
+    }
+    vault = {
+      instance_type        = "t3.small"
+      policy_resource_list = []
     }
   }
+
 }
 variable "zone_id" {
   default = "Z01666541S6XI9QO852LS"
